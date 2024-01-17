8ce0a13fdafd810842066a4047b1eab5e594294a
Kishibae <ash3082@gmail.com>
2024-01-17 05:06:11 +0000

    first

---
+++ paytest/main/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+"""
+class Member(models.Model):
+     Fkone = models.ForeignKey(User,on_delete=models.CASADE)
+     Fktwo = models.ForeignKey(Event,on_delete=models.CASADE)
+     
+     
+
+class Event(models.Model):
+     subject = models.CharField(max_length=128)
+     create_date = models.DateField()
+     
+     def __str__(self):
+        return self.subject
+       
+
+     
+"""
+    
